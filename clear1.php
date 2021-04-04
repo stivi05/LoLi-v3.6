429b5da2264a9d6b89880febf340831879940b42
lolyworlds <lolyworlds@gmail.com>
2021-04-04 11:14:46 +0200

    Update clear1.php

--- clear1.php
+++ clear1.php
@@ -1,6 +1,6 @@
 <?php require_once("include/bittorrent.php");dbconn(true);gzip();if ($CURUSER && get_user_class() >= UC_ADMINISTRATOR){
 $action = ($_POST['action'] ? $_POST['action'] : ($_GET['action'] ? $_GET['action'] : 'start')); 
-if($action == 'clearshout'){sql_query('TRUNCATE TABLE shoutbox2') or sqlerr(__FILE__, __LINE__);header("Refresh: 3; url=chat");
+if($action == 'clearshout'){sql_query('TRUNCATE TABLE shoutbox2') or sqlerr(__FILE__, __LINE__);header("Refresh: 3; url=chat.php");
 stderr2('<center><b>Очистка прошла успешно.</b></center>','<center><b>Таблица чата была успешно очищена. Сейчас вы будете возвращены в чат.</b></center>'); 
 }elseif($action == 'start'){ 
 stderr2('<center><b>Очистить таблицы</b></center>','<center>Нажмите <a href="'.$_SERVER['SCRIPT_NAME'].'?action=clearshout">сюда</a> что-бы очистить таблицы.</center>',false); 
