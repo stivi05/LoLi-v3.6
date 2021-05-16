88ba15221eb3c482ecef57d036cea29b9efbce37
lolyworlds <lolyworlds@gmail.com>
2021-05-16 10:50:57 +0200

    Update bans.sql
    Модифицированная таблица банов

--- SQL/bans.sql
+++ SQL/bans.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `bans` (
   `id` smallint(5) unsigned NOT NULL,
   `added` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `addedby` tinyint(2) unsigned NOT NULL DEFAULT '0',
-  `comment` varchar(40) NOT NULL DEFAULT '',
+  `comment` varchar(150) NOT NULL DEFAULT '',
   `first` varchar(25) DEFAULT NULL,
   `haker` enum('yes','no') DEFAULT 'no'
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
