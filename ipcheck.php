5130d6d438348d4205262fbccfc0fa73a9413ad8
lolyworlds <lolyworlds@gmail.com>
2021-05-16 08:59:26 +0200

    Update ipcheck.php
    Чуть переделал запросы, обрезал лишнее. Страница повторных IP, выбирает ВСЕ, даже забаненные / отключенные аки.
    Идет в комплекте с банами - смотрите страницу банов и в SQL запрос на новую таблицу банов.
    Так-же нужно запрос на два поля для подсчета сидируемого и качаемого юзером, поставьте этот мод: https://tbdev-forum.top/viewtopic.php?f=53&t=276

--- ipcheck.php
+++ ipcheck.php
@@ -13,11 +13,11 @@ print("<table style='background:none;width:100%;border:0;'><tr><td class='zaliwk
 while($ras = mysql_fetch_assoc($res)){if($ras["dupl"] <= 1)break;
 $ros = sql_query("SELECT id, username, class, email, added, last_access, downloaded, uploaded, warned, donor, enabled, (seeder + leecher) AS peer_count 
 FROM users WHERE ip='".$ras['ip']."' ORDER BY id DESC") or sqlerr(__FILE__, __LINE__);while($arr = mysql_fetch_assoc($ros)){
-if($arr['added'] == '0000-00-00 00:00:00')$arr['added'] = 'никогда';if($arr['last_access'] == '0000-00-00 00:00:00')$arr['last_access'] = 'никогда';
+if($arr['added'] == '0000-00-00 00:00:00')$arr['added'] = '-';if($arr['last_access'] == '0000-00-00 00:00:00')$arr['last_access'] = '-';
 if($arr["downloaded"] != 0)$ratio = number_format($arr["uploaded"] / $arr["downloaded"], 3);else $ratio = "inf.";
 $ratio = "<font color=".get_ratio_color($ratio).">$ratio</font>";$uploaded = mksize($arr["uploaded"]);$downloaded = mksize($arr["downloaded"]);
 $added = substr($arr['added'], 0, 10);$last_access = substr($arr['last_access'], 0, 10);
-print("<tr><td align='center'><b><a href='user_".$arr['id']."'>".get_user_class_color($arr['class'], $arr['username'])."</b></a>".get_user_icons($arr)."</td>
+print("<tr><td align='center'><b><a href='userdetails.php?id=".$arr['id']."'>".get_user_class_color($arr['class'], $arr['username'])."</b></a>".get_user_icons($arr)."</td>
 <td align='center'>$arr[email]</td><td align='center'>$added</td><td align='center'>$last_access</td><td align='center'>$downloaded</td><td align='center'>$uploaded</td>
 <td align='center'>$ratio</td><td align='center'><span style='font-weight:bold;'>".$ras['ip']."</span></td><td align='center'>
 ".($arr['peer_count'] > 0 ? "<span style='color:red;font-weight:bold;'>Да</span>" : "<span style='color:green;font-weight:bold;'>Нет</span>")."</td></tr>");
