4ee58fd29c0f9e7586fbff55b0a584a9df2a4d11
lolyworlds <lolyworlds@gmail.com>
2021-06-04 15:24:56 +0200

    Update block-heroes.php
    Блок Наши Герои на кеше файловом.
    Все описано тут: https://tbdev-forum.top/viewtopic.php?f=53&t=522
    Сделать запросы и вставить код в клинап.
    -------------
    Поправил стиль букв на Джорджия, ибо на мобильных плохо показывает китайский шрифт.

--- include/blocks/block-heroes.php
+++ include/blocks/block-heroes.php
@@ -6,7 +6,7 @@ $content .= "<table style='background:none;width:100%;float:center;border:0;'><t
 <tr><td class='zaliwka' colspan='3' style='color:#FFFFFF;height:15px;font-weight:bold;text-align:center;border:0;border-radius:5px;'>Бонусы</td></tr>";
 $bonus = sql_query("SELECT bonus, id, class, username FROM users WHERE bonus > 1000 ORDER BY bonus DESC LIMIT 5") or sqlerr(__FILE__, __LINE__);
 while($bonus2 = mysql_fetch_array($bonus)){$id = $bonus2["id"];$bonus3 = $bonus2["bonus"];
-$content .= "<tr><td style='margin-top:3px;border-radius:8px;font-family:fangsong;-webkit-border-radius:8px;-moz-border-radius:8px;-khtml-border-radius:8px;border:1px solid #E0FFFF;display:block;' class='b'>
+$content .= "<tr><td style='margin-top:3px;border-radius:8px;font-family:Georgia;-webkit-border-radius:8px;-moz-border-radius:8px;-khtml-border-radius:8px;border:1px solid #E0FFFF;display:block;' class='b'>
 &nbsp;&nbsp;<a href='userdetails.php?id=$id'>".get_user_class_color($bonus2["class"], $bonus2["username"])."</a></td>
 <td width='3px' align='center' valign='top' style='background:none;border:none;'></td>
 <td style='margin-top:3px;border-radius:8px;font-family:monospace;-webkit-border-radius:8px;font-size:10px;font-weight:bold;text-align:center;-moz-border-radius:8px;-khtml-border-radius:8px;border:1px solid #E0FFFF;display:block;' class='b'>
@@ -17,7 +17,7 @@ $content .= "</table></td><td width='5px' align='center' valign='top' style='bac
 $sym = sql_query("SELECT simpaty, id, class, username FROM users WHERE simpaty >= 1 ORDER BY simpaty DESC LIMIT 5") or sqlerr(__FILE__, __LINE__);
 while ($sym2 = mysql_fetch_array($sym)){$id = $sym2["id"];$sym3 = $sym2["simpaty"];
 $content .= "<tr>
-<td style='margin-top:3px;border-radius:8px;font-family:fangsong;-webkit-border-radius:8px;-moz-border-radius:8px;-khtml-border-radius:8px;border:1px solid #E0FFFF;display:block;' class='b'>
+<td style='margin-top:3px;border-radius:8px;font-family:Georgia;-webkit-border-radius:8px;-moz-border-radius:8px;-khtml-border-radius:8px;border:1px solid #E0FFFF;display:block;' class='b'>
 &nbsp;&nbsp;<a href='userdetails.php?id=$id'>".get_user_class_color($sym2["class"], $sym2["username"])."</a></td>
 <td width='3px' align='center' valign='top' style='background:none;border:none;'></td>
 <td style='margin-top:3px;border-radius:8px;font-family:monospace;-webkit-border-radius:8px;font-size:10px;font-weight:bold;text-align:center;-moz-border-radius:8px;-khtml-border-radius:8px;border:1px solid #E0FFFF;display:block;' class='b'>
@@ -28,7 +28,7 @@ $content .= "</table></td><td width='5px' align='center' valign='top' style='bac
 $comm = sql_query("SELECT comreliz, id, class, username FROM users WHERE comreliz > 1 ORDER BY comreliz DESC LIMIT 5") or sqlerr(__FILE__, __LINE__);
 while ($comm2 = mysql_fetch_array($comm)){$id = $comm2["id"];$comm3 = $comm2["comreliz"];
 $content .= "<tr>
-<td style='margin-top:3px;border-radius:8px;font-family:fangsong;-webkit-border-radius:8px;-moz-border-radius:8px;-khtml-border-radius:8px;border:1px solid #E0FFFF;display:block;' class='b'>
+<td style='margin-top:3px;border-radius:8px;font-family:Georgia;-webkit-border-radius:8px;-moz-border-radius:8px;-khtml-border-radius:8px;border:1px solid #E0FFFF;display:block;' class='b'>
 &nbsp;&nbsp;<a href='userdetails.php?id=$id'>".get_user_class_color($comm2["class"], $comm2["username"])."</a></td>
 <td width='3px' align='center' valign='top' style='background:none;border:none;'></td>
 <td style='margin-top:3px;border-radius:8px;font-family:monospace;-webkit-border-radius:8px;font-size:10px;font-weight:bold;text-align:center;-moz-border-radius:8px;-khtml-border-radius:8px;border:1px solid #E0FFFF;display:block;' class='b'>
@@ -39,7 +39,7 @@ $content .= "</table></td><td width='5px' align='center' valign='top' style='bac
 $reliz = sql_query("SELECT relizs, id, class, username FROM users WHERE relizs > 1 ORDER BY relizs DESC LIMIT 5") or sqlerr(__FILE__, __LINE__);
 while ($relizs = mysql_fetch_array($reliz)){$id = $relizs["id"];$relizsz = $relizs["relizs"];
 $content .= "<tr>
-<td style='margin-top:3px;border-radius:8px;font-family:fangsong;-webkit-border-radius:8px;-moz-border-radius:8px;-khtml-border-radius:8px;border:1px solid #E0FFFF;display:block;' class='b'>
+<td style='margin-top:3px;border-radius:8px;font-family:Georgia;-webkit-border-radius:8px;-moz-border-radius:8px;-khtml-border-radius:8px;border:1px solid #E0FFFF;display:block;' class='b'>
 &nbsp;&nbsp;<a href='userdetails.php?id=$id'>".get_user_class_color($relizs["class"], $relizs["username"])."</a></td>
 <td width='3px' align='center' valign='top' style='background:none;border:none;'></td>
 <td style='margin-top:3px;border-radius:8px;font-family:monospace;-webkit-border-radius:8px;font-size:10px;font-weight:bold;text-align:center;-moz-border-radius:8px;-khtml-border-radius:8px;border:1px solid #E0FFFF;display:block;' class='b'>
