7d72fddbf4affc4b719695978d504e0560958854
lolyworlds <lolyworlds@gmail.com>
2021-06-06 00:23:07 +0200

    Update staff.php
    Новый вид странички "Команда сайта". Новый дизайн, файловый кеш на 30 минут.
    =============
    Обновил код и немного дизайн поправил - от 6 Июня 2021 года
    =============
    ---------------------
    У вас должна быть установлена система файлового кеширования, вот эта: https://tbdev-forum.top/viewtopic.php?f=53&t=4
    --------------------------------

--- staff.php
+++ staff.php
@@ -11,11 +11,12 @@
 <table style="margin-top:5px;background:none;cellspacing:0;cellpadding:0;width:100%;float:center;"><?
 global $CacheBlock, $staffBlock_Refresh, $staffsBlock_Refresh;$_cache = 'staff.cache';$_cacher = 'staffs.cache';
 if(!$CacheBlock->Check($_cache, $staffBlock_Refresh?0:3600)){$dt = gmtime() - 300;$dt = sqlesc(get_date_time($dt));
-$res = sql_query("SELECT users.id, users.avatar, users.country, users.gender, users.username, users.class, users.last_access, c.name, c.flagpic FROM users LEFT JOIN countries AS c ON c.id = users.country WHERE users.class>=".UC_UPLOADER." AND users.status='confirmed' ORDER BY users.username" ) or sqlerr(__FILE__, __LINE__);
+$res = sql_query("SELECT users.id, users.avatar, users.country, users.gender, users.username, users.class, users.last_access, c.name, c.flagpic FROM users 
+LEFT JOIN countries AS c ON c.id = users.country WHERE users.class>=".UC_UPLOADER." AND users.status='confirmed' ORDER BY users.username" ) or sqlerr(__FILE__, __LINE__);
 while($arr = mysql_fetch_assoc($res)){
 if($arr['country'] > 0){$country = "<img src='pic/flag/".$arr['flagpic']."' alt='".$arr['name']."' title='".$arr['name']."'/>";}else{$country = "<Не указанно";}
-if(!$arr['avatar']){$avatar=("<a href='userdetails.php?id=".$arr['id']."'><img height='50px' border='0' title='".$arr['username']."' src='pic/default_avatar.gif'/></a>");}
-else{$avatar=("<a href='userdetails.php?id=".$arr['id']."'><img height='50px' src='".$arr['avatar']."' title='".$arr['username']."'/></a>");}
+if(!$arr['avatar']){$avatar=("<a href='userdetails.php?id=".$arr['id']."'><img style='border-radius:5px;border:0;height:50px;' title='".$arr['username']."' src='pic/default_avatar.gif'/></a>");}
+else{$avatar=("<a href='userdetails.php?id=".$arr['id']."'><img style='border-radius:5px;border:0;height:50px;' src='".$arr['avatar']."' title='".$arr['username']."'/></a>");}
 if($arr['gender'] == '1'){$gender = "<img src='pic/male1.gif' alt='Парень' style='margin-left:4pt;'/>";}
 elseif($arr['gender'] == '2'){$gender = "<img src='pic/female1.gif' alt='Девушка' style='margin-left:4pt;'/>";}
 elseif($arr['gender'] == '3'){$gender = "<img src='pic/transgenders.gif' alt='Не определился' style='margin-left:4pt;'/>";}
@@ -26,26 +27,27 @@ $staff_table[$arr['class']] = $staff_table[$arr['class']]."<td style='padding:5p
 <a class='altlink' href='userdetails.php?id=".$arr['id']."'><b>".get_user_class_color($arr['class'],$arr['username'])."</b></a>".($arr["donated"] > 0 ? "
 <img src='pic/star.gif' border='0' alt='Donor'/>" : "")."&nbsp;&nbsp;$gender<hr width='140px'>
 ".("'".$arr['last_access']."'">$dt?"<img src='pic/button_online.gif' border='0' alt='online'/>":"<img src='pic/button_offline.gif' border='0' alt='offline'/>" )."&nbsp;&nbsp;
-<a href='#' onclick=\"javascript:window.open('sendpm.php?action=sendmessage&receiver=".$arr['id']."', 'Отправить PM', 'width=650px, height=465px');return false;\" title='Отправить ЛС'><img src='pic/pn_inbox.gif' border='0' title='PM'/></a>&nbsp;&nbsp;$country</center></td></tr></table><hr width='200px'><table style='background:none;border:0;' width='240'><center>
+<a href='#' onclick=\"javascript:window.open('sendpm.php?action=sendmessage&receiver=".$arr['id']."', 'Отправить PM', 'width=650px, height=465px');return false;\" title='Отправить ЛС'><img src='pic/pn_inbox.gif' border='0' title='PM'/></a>
+&nbsp;&nbsp;$country</center></td></tr></table><hr width='200px'><table style='background:none;border:0;' width='240px'><center>
 <b>Последний визит:</b>&nbsp;<font color='blue'><b>".get_elapsed_time(sql_timestamp_to_unix_timestamp($arr["last_access"]))." ".$tracker_lang['ago']."</b></font></center></table>
 </td></tr></table></td>";
 ++$col[$arr['class']];if($col[$arr['class']]<=4) $staff_table[$arr['class']]=$staff_table[$arr['class']]."";
-else{$staff_table[$arr['class']]=$staff_table[$arr['class']]."</tr><tr height='15'>";$col[$arr['class']]=0;}}
+else{$staff_table[$arr['class']]=$staff_table[$arr['class']]."</tr><tr>";$col[$arr['class']]=0;}}
 $content.= "<tr><td style='border-radius:8px;-webkit-border-radius:8px;-moz-border-radius:8px;-khtml-border-radius:8px;border:1px solid #E0FFFF;display:block;' class='a'>
 <table style='background:none;width:100%;float:center;border:0;'><tr><td class='zaliwka' style='colspan:14;height:20px;;border:0;border-radius:5px;'>
 <table style='background:none;width:100%;float:center;border:0;'><tr>
 <td style='background:none;font-family:tahoma;width:100%;font-size:14px;font-weight:bold;color:#FFFFFF;margin-left:10px;letter-spacing:0;text-align:center;float:center;border:0;'>
 .:: Администрация ::.</td></tr></table></td></tr><tr><td align='left' style='background:none;width:100%;float:center;border:0;'></td></tr></table>
-</td></tr></table><table style='background:none;margin-top:3px;cellspacing:0;cellpadding:0;width:100%;float:center;'><tr height='15'>".$staff_table[UC_SYSOP]."</tr>";
-if($staff_table[UC_ADMINISTRATOR]){$content.= "<tr height='15'>".$staff_table[UC_ADMINISTRATOR]."</tr>";}
-if($staff_table[UC_MODERATOR]){$content.= "<tr height='15'>".$staff_table[UC_MODERATOR]."</tr>";}
+</td></tr></table><table style='background:none;margin-top:3px;cellspacing:0;cellpadding:0;width:100%;float:center;'><tr>".$staff_table[UC_SYSOP]."</tr>";
+if($staff_table[UC_ADMINISTRATOR]){$content.= "<tr>".$staff_table[UC_ADMINISTRATOR]."</tr>";}
+if($staff_table[UC_MODERATOR]){$content.= "<tr>".$staff_table[UC_MODERATOR]."</tr>";}
 $content.= "</table><table style='background:none;margin-top:3px;cellspacing:0;cellpadding:0;width:100%;float:center;'>
 <tr><td style='border-radius:8px;-webkit-border-radius:8px;-moz-border-radius:8px;-khtml-border-radius:8px;border:1px solid #E0FFFF;display:block;' class='a'>
 <table style='background:none;width:100%;float:center;border:0;'><tr><td class='zaliwka' style='colspan:14;height:20px;;border:0;border-radius:5px;'>
 <table style='background:none;width:100%;float:center;border:0;'><tr>
 <td style='background:none;font-family:tahoma;width:100%;font-size:14px;font-weight:bold;color:#FFFFFF;margin-left:10px;letter-spacing:0;text-align:center;float:center;border:0;'>
 .:: Аплоадеры ::.</td></tr></table></td></tr><tr><td align='left' style='background:none;width:100%;float:center;border:0;'></td></tr></table></td></tr>
-</table><table style='background:none;margin-top:3px;cellspacing:0;cellpadding:0;width:100%;float:center;'><tr height='15'>".$staff_table[UC_UPLOADER]."</tr>";
+</table><table style='background:none;margin-top:3px;cellspacing:0;cellpadding:0;width:100%;float:center;'><tr>".$staff_table[UC_UPLOADER]."</tr>";
 $CacheBlock->Write($_cache, $content);}else $content = $CacheBlock->Read($_cache);print ($content."</table>");?>
 <table style="margin-top:5px;background:none;cellspacing:0;cellpadding:0;width:100%;float:center;"><tr>
 <td style='border-radius:8px;-webkit-border-radius:8px;-moz-border-radius:8px;-khtml-border-radius:8px;border:1px solid #E0FFFF;display:block;' class='a'>
@@ -60,8 +62,8 @@ $resd = sql_query("SELECT users.id, users.username, users.gender, users.class, u
 FROM users LEFT JOIN countries AS c ON c.id = users.country WHERE users.support='yes' AND users.status='confirmed' ORDER BY users.username LIMIT 10") or sqlerr(__FILE__, __LINE__);
 while ($arr = mysql_fetch_assoc($resd)){
 if($arr['country'] > 0){$country = "<img src='pic/flag/".$arr['flagpic']."' alt='".$arr['name']."' title='".$arr['name']."'/>";}else{$country = "<Не указанно";}
-if(!$arr['avatar']){$avatar=("<a href='userdetails.php?id=".$arr['id']."'><img height='50px' border='0' title='".$arr['username']."' src='pic/default_avatar.gif'/></a>");}
-else{$avatar=("<a href='userdetails.php?id=".$arr['id']."'><img height='50px' src='".$arr['avatar']."' title='".$arr['username']."'/></a>");}
+if(!$arr['avatar']){$avatar=("<a href='userdetails.php?id=".$arr['id']."'><img style='border-radius:5px;border:0;height:50px;' title='".$arr['username']."' src='pic/default_avatar.gif'/></a>");}
+else{$avatar=("<a href='userdetails.php?id=".$arr['id']."'><img style='border-radius:5px;border:0;height:50px;' src='".$arr['avatar']."' title='".$arr['username']."'/></a>");}
 if($arr['gender'] == '1'){$gender = "<img src='pic/male1.gif' alt='Парень' style='margin-left:4pt;'/>";}
 elseif($arr['gender'] == '2'){$gender = "<img src='pic/female1.gif' alt='Девушка' style='margin-left:4pt;'/>";}
 elseif($arr['gender'] == '3'){$gender = "<img src='pic/transgenders.gif' alt='Не определился' style='margin-left:4pt;'/>";}
