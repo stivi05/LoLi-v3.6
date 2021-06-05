ee93197b1d59b900b6d125b501f99c28a7f70dc2
lolyworlds <lolyworlds@gmail.com>
2021-06-06 00:09:41 +0200

    Update users.php
    users.php + users_suggest.php  -> Эти две странички идут вместе!
    Это страничка Пользователей сайта и быстрого поиска-отображения юзера в поисковой строчке на странице Юзеры.
    Стоит защита от взлома и шела. Код поправлен под HTML5.
    Собственная наработка и внешний вид!
    ----------------
    Поправил вид аватаров и чутка код под ваши файлы, без сокращения названия файлов
    ----------------

--- users.php
+++ users.php
@@ -25,7 +25,9 @@ $res = sql_query("SELECT COUNT(id) FROM users$query") or sqlerr(__FILE__, __LINE
 $perpage = 12;list($pagertop, $pagerbottom, $limit) = pager2($perpage, $count, "users?".$q);
 $res = sql_query("SELECT u.*, c.name, c.flagpic FROM users AS u LEFT JOIN countries AS c ON c.id = u.country$query ORDER BY added $limit") or sqlerr(__FILE__, __LINE__);
 $num = mysql_num_rows($res);?></td></tr></table></td></tr></table><?if(mysql_num_rows($res) == 0){
-echo "<table style='background:none;border:none;cellspacing:0;cellpadding:0;margin-top:7px;width:200px;float:center;'><tr><td style='margin-top:7px;width:250px;float:center;border-radius:5px;-webkit-border-radius:5px;-moz-border-radius:5px;-khtml-border-radius:5px;border:1px solid white;display:block;' class='a'><center><font style='font-family:tahoma;font-size:14px;font-weight:10;color:red;'><b>НЕТ юзеров по вашему запросу!</b></font></center></td></tr></table>";
+echo "<table style='background:none;border:none;cellspacing:0;cellpadding:0;margin-top:7px;width:200px;float:center;'><tr>
+<td style='margin-top:7px;width:250px;float:center;border-radius:5px;-webkit-border-radius:5px;-moz-border-radius:5px;-khtml-border-radius:5px;border:1px solid white;display:block;' class='a'>
+<center><font style='font-family:tahoma;font-size:14px;font-weight:10;color:red;'><b>НЕТ юзеров по вашему запросу!</b></font></center></td></tr></table>";
 }else{?><table style="margin-top:7px;background:none;cellspacing:0;cellpadding:0;width:100%;float:center;align:center;border:0;">
 <tr><td align='center' style='background:none;cellspacing:0;cellpadding:0;width:100%;float:center;'><?=$pagertop?></td></tr></table>
 <table style="background:none;cellspacing:0;cellpadding:0;width:100%;float:center;align:center;border:0;"><?$nc=1;for($i = 0; $i < $num; ++$i){
@@ -38,16 +40,18 @@ $ratio = "<font color='".get_ratio_color($ratio)."'>$ratio</font>";}elseif($arr[
 if($arr["gender"] == "1") $gender = "<img src='pic/male1.gif' alt='Парень' style='margin-left:4px;'/>";
 elseif($arr["gender"] == "2") $gender = "<img src='pic/female1.gif' alt='Девушка' style='margin-left:4pt;'/>";
 elseif($arr["gender"] == "3") $gender = "<img src='pic/transgenders.gif' alt='Трансгендер' style='margin-left:4pt;'/>";
-if(!$arr[avatar]){$avatar=("<a href='user_".$arr['id']."'><img border='0' height='80px' title='".$arr['username']."' src='pic/default_avatar.gif'></a>");
-}else{$avatar=("<a href='user_".$arr['id']."'><img height='80px' src='".$arr['avatar']."' title='".$arr['username']."'/></a>");}
+if(!$arr[avatar]){$avatar=("<a href='userdetails.php?id=".$arr['id']."'><img style='border-radius:5px;border:0;height:80px;' title='".$arr['username']."' src='pic/default_avatar.gif'></a>");
+}else{$avatar=("<a href='userdetails.php?id=".$arr['id']."'><img style='border-radius:5px;border:0;height:80px;' src='".$arr['avatar']."' title='".$arr['username']."'/></a>");}
 print("<td style='padding:10px;width:240px;background:none;'><table style='background:none;'border='0' cellspacing='0'><tr>
-<td style='border-radius:10px;-webkit-border-radius:8px;-moz-border-radius:8px;-khtml-border-radius:8px;border:2px solid #E0FFFF;display:block;padding:10px;width:240px;' class='a'><table style='background:none;align:center;border:0;' width='240px'><tr><td class='embedded' width='90px' style='background:none;align:center;border:0;'><center>$avatar
-</center></td><td class='embedded' valign='top' align='center' style='background:none;border:0;'><center>
-<a class='altlink' href='user_".$arr['id']."'><b>".get_user_class_color($arr['class'],$arr['username'])."</b></a>".($arr["donated"] > 0 ? "
+<td style='border-radius:10px;-webkit-border-radius:8px;-moz-border-radius:8px;-khtml-border-radius:8px;border:2px solid #E0FFFF;display:block;padding:10px;width:240px;' class='a'>
+<table style='background:none;align:center;border:0;' width='240px'><tr><td class='embedded' width='90px' style='background:none;align:center;border:0;'><center>$avatar</center>
+</td><td class='embedded' valign='top' align='center' style='background:none;border:0;'><center>
+<a class='altlink' href='userdetails.php?id=".$arr['id']."'><b>".get_user_class_color($arr['class'],$arr['username'])."</b></a>".($arr["donated"] > 0 ? "
 <img src='pic/star.gif' border='0' alt='Donor'/>" : "")."&nbsp;&nbsp;$gender<hr width='100px'>
 ".("'".$arr['last_access']."'">$dt?"<img src='pic/button_online.gif' border='0' alt='online'/>":"<img src='pic/button_offline.gif' border='0' alt='offline'/>" )."&nbsp;&nbsp;
-<a href='#' onclick=\"javascript:window.open('sendpm_".$arr['id']."', 'Отправить PM', 'width=650, height=465');return false;\" title='Отправить ЛС'><img src='pic/pn_inbox.gif' border='0' title='PM'/></a>&nbsp;&nbsp;$country<br><br><img src='pic/multitracker.png' border='0' alt='Рейтинг'/>&nbsp;<b>$ratio</b></center></td></tr></table><hr width='200px'>
-<table style='background:none;border:0;' width='240'><tr><td class='embedded' align='left' style='background:none;border:0;'>
+<a href='#' onclick=\"javascript:window.open('sendpm.php?action=sendmessage&receiver=".$arr['id']."', 'Отправить PM', 'width=650px, height=465px');return false;\" title='Отправить ЛС'><img src='pic/pn_inbox.gif' border='0' title='PM'/></a>
+&nbsp;&nbsp;$country<br><br><img src='pic/multitracker.png' border='0' alt='Рейтинг'/>&nbsp;<b>$ratio</b></center></td></tr></table><hr width='200px'>
+<table style='background:none;border:0;' width='240px'><tr><td class='embedded' align='left' style='background:none;border:0;'>
 <b>Зарегистрирован:</b>&nbsp;<font color='green'><b>".get_elapsed_time(sql_timestamp_to_unix_timestamp($arr["added"]))." ".$tracker_lang['ago']."</b>
 </font></td></tr><tr><td class='embedded' width='150px' align='left' style='background:none;border:0;'><b>Последний визит:</b>&nbsp;<font color='blue'>
 <b>".get_elapsed_time(sql_timestamp_to_unix_timestamp($arr["last_access"]))." ".$tracker_lang['ago']."</b></font></td></tr></table></td></tr></table></td>");
