d4ff5cad99e1707db589ee10721bf1d561b1856a
lolyworlds <lolyworlds@gmail.com>
2021-07-26 22:30:48 +0300

    Add files via upload
    Смена Класса Юзера. Доступ от Модератор и Выше. На МЕМКЕШ !

--- restoreclass.php
+++ restoreclass.php
@@ -1,5 +1,4 @@
-<?php require_once("include/bittorrent.php");dbconn(true);gzip();if($CURUSER && $CURUSER['override_class'] != 255){global $rootpath;
-mysql_query("UPDATE users SET override_class = 255 WHERE id = ".$CURUSER['id']);$flist = $rootpath."include/user_cache/user_".$CURUSER["id"].".cache";
-if(file_exists($flist)){unlink($rootpath."include/user_cache/user_".$CURUSER["id"].".cache");}
-header("Refresh: 0; url=/");}else{?><html><head><meta http-equiv='refresh' content='0;url=/'></head>
+<?php require_once("include/bittorrent.php");dbconn(true);gzip();if($CURUSER && $CURUSER['override_class'] != 255){
+mysql_query("UPDATE users SET override_class = 255 WHERE id = ".$CURUSER['id']);$cache = new Memcache();$cache->connect('127.0.0.1', 11211); //прописать IP вашего сервера!//
+$cache->delete('user_cache_'.$CURUSER['id']);header("Refresh: 0; url=/");}else{?><html><head><meta http-equiv='refresh' content='0;url=/'></head>
 <body style="background:#2F4F4F no-repeat center center fixed;-webkit-background-size:cover;-moz-background-size:cover;-o-background-size:cover;background-size:cover;"></body></html><?}?>
