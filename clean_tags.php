9bea329681f31c02a6cd6d27b588d4981bf8d249
lolyworlds <lolyworlds@gmail.com>
2022-01-07 19:54:46 +0200

    Update clean_tags.php
    файл обновления ТЕГОВ и таблицы Бровс (торренты)

--- clean_tags.php
+++ clean_tags.php
@@ -1,11 +1,11 @@
 <?php require_once("include/bittorrent.php");dbconn(true);gzip();if ($CURUSER && get_user_class() >= UC_ADMINISTRATOR){
 $action = ($_POST['action'] ? $_POST['action'] : ($_GET['action'] ? $_GET['action'] : 'start')); 
 if($action == 'clean_tags'){sql_query('TRUNCATE TABLE tags') or sqlerr(__FILE__, __LINE__);sql_query('TRUNCATE TABLE browse') or sqlerr(__FILE__, __LINE__);
-sql_query('INSERT INTO tags (id, name, keywords, description, not_sticky, image1, free, multitracker, seeders, added) SELECT id, name, keywords, description, 
-not_sticky, image1, free, multitracker, seeders, added FROM torrents') or sqlerr(__FILE__, __LINE__);
+sql_query('INSERT INTO tags (id, name, keywords, description, not_sticky, free, multitracker, seeders, added) SELECT id, name, keywords, description, 
+not_sticky, free, multitracker, seeders, added FROM torrents') or sqlerr(__FILE__, __LINE__);
 sql_query('INSERT INTO browse (id, name, description, category, incategory, voice, size, added, comments, leechers, remote_leechers, seeders, remote_seeders, 
-owner, free, not_sticky, multitracker, dostup, updatess) SELECT id, name, description, category, incategory, size, added, comments, leechers, 
-remote_leechers, seeders, remote_seeders, owner, free, not_sticky, multitracker, dostup, updatess FROM torrents') or sqlerr(__FILE__, __LINE__);
+owner, free, not_sticky, multitracker, updatess) SELECT id, name, description, category, incategory, size, added, comments, leechers, 
+remote_leechers, seeders, remote_seeders, owner, free, not_sticky, multitracker, updatess FROM torrents') or sqlerr(__FILE__, __LINE__);
 header("Refresh: 3; url=browse");
 stderr2('<center><b>Таблицы тегов и торрентов были успешно очищены и обновлены.</b></center>','<center><b>Таблица Тегов и Торрентов были успешно очищены и обновлены. Сейчас вы будете возвращены в Browse.</b></center>'); 
 }elseif($action == 'start'){ 
