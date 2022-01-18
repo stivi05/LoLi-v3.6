b5bd1ad505d2611fd23bbafbd69fff8eaac80c24
lolyworlds <lolyworlds@gmail.com>
2022-01-18 14:35:00 +0200

    Add files via upload
    Релиз-группы, как и Категории / Подкатегории. Можно делать сколько угодно РГ и выводить их в деталях, бровс и т.д. (хотя я бы рекомендовал только в деталях, меньше запросов будет).
    К таблице идет файл создания и управления группами - reliz.php (в корне сайта). Доступ от Администратора и выше.

---
+++ SQL_BD/reliz.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS `reliz` (
+  `id` tinyint(1) unsigned NOT NULL,
+  `sort` tinyint(1) NOT NULL DEFAULT '0',
+  `name` varchar(12) NOT NULL DEFAULT '',
+  `image` varchar(12) NOT NULL DEFAULT ''
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+ALTER TABLE `reliz`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`sort`,`id`,`name`) USING BTREE;
+
+ALTER TABLE `reliz`
+  MODIFY `id` tinyint(1) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
