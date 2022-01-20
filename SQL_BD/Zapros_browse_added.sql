632a923deea226fbfc73bacd5662e28c83a94009
lolyworlds <lolyworlds@gmail.com>
2022-01-20 10:49:33 +0200

    Delete Zapros_browse_added.sql

--- SQL_BD/Zapros_browse_added.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE browse
-SELECT id, name, description, category, incategory, voice, tryd, webdl, reliz, size, added, comments, leechers, remote_leechers, seeders, remote_seeders, owner, free, not_sticky, multitracker, dostup, updatess FROM torrents
